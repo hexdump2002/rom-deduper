c16e37e902930b677784b2001aec40817f74ad8b
hexdump <hexdump2002@yahoo.es>
2023-07-17 16:12:02 +0200

    Solved bugs for the new file distribution

--- deduper.py
+++ deduper.py
@@ -5,6 +5,7 @@ import pathlib
 import re
 import shutil
 import os.path as p
+import time
 from typing import List
 
 from enum import Enum
@@ -12,7 +13,6 @@ from enum import Enum
 import xml.etree.ElementTree as ET
 from fuzzywuzzy import fuzz
 
-fileGroups=[]
 
 class Find(Enum):
     WORD = 0
@@ -40,12 +40,12 @@ class GameGroup:
 
         bestVer:GameFile = self.games[0]
 
-        bestVerRating= getRatingForGameName(bestVer.baseName)
+        bestVerRating= _getRatingForGameName(bestVer.baseName)
         #print("*Checking: %s -> %s" % (bestVer,bestVerRating))
 
         restOfFiles:List[GameFile] = self.games[1:]
         for game in restOfFiles:
-            currentVerRating = getRatingForGameName(game.baseName)
+            currentVerRating = _getRatingForGameName(game.baseName)
             #print("*Checking: %s -> %s" % (game.absPath, currentVerRating))
             if currentVerRating > bestVerRating:
                 bestVer = game
@@ -55,6 +55,13 @@ class GameGroup:
 
         return bestVer
 
+    def getNonBestVersionGames(self):
+        bestVersion:GameFile = self.getBestVersion()
+
+        gamesToRemove:[] = [*self.games]
+        gamesToRemove.remove(bestVersion)
+
+        return gamesToRemove
 
 class Comparers:
 
@@ -71,13 +78,13 @@ class Comparers:
 
 
 
-def  getNameToCompare(name:str, removeCharsFromStartCount:int):
+def  _getNameToCompare(name:str, removeCharsFromStartCount:int):
     name = name[removeCharsFromStartCount:]
     name=name.replace("'","_") #do this because some sets come with these to symbols to diferentiate versions
     parts = name.split("(")
     return parts[0].strip()
 
-def findWords(searchWords:[[]], text, trueIfAll=False):
+def _findWords(searchWords:[[]], text, trueIfAll=False):
 
     found = False
     for search in searchWords:
@@ -103,66 +110,66 @@ def findWords(searchWords:[[]], text, trueIfAll=False):
 
     return found
 
-def getRatingForGameName(name):
+def _getRatingForGameName(name):
     points = 0
 
     name = name.lower()
 
     isJap=False
-    if findWords([["europe",Find.WORD],["eur",Find.WORD],["\(E\)",Find.REGEX]], name): points += 50
-    if findWords([["japan",Find.WORD],["jap",Find.WORD],["\(J\)",Find.REGEX]], name):   points += 10; isJap=True
-    if findWords([["usa",Find.WORD], ["\(U\)",Find.REGEX]], name): points += 30
-    if findWords([["world",Find.WORD]], name): points += 20
-    if findWords([["beta",Find.WORD]],name): points -=30
-    if findWords([["\(proto.*?\)",Find.REGEX]], name): points -= 50
-    if findWords([["spain",Find.WORD]],name): points += 100
-    if findWords([["\[!\]",Find.REGEX]],name): points += 40
-    if findWords([["\[b\d{0,2}\]",Find.REGEX]], name): points -= 40
-    if findWords([["\[h\d{0,2}\]",Find.REGEX]], name): points -= 100
-    if findWords([["\(UE\)",Find.REGEX],["\(EU\)",Find.REGEX]],name): points+=50
-    if findWords([["\(UJ\)",Find.REGEX],["(JU)",Find.REGEX]], name): points += 30
-    if findWords([["\[a\d{0,2}(?![-\w])\]",Find.REGEX]],name): points-=10
-    if findWords([["\[o\d{0,2}(?![-\w])\]", Find.REGEX]], name): points -= 10 #Overdump
+    if _findWords([["europe", Find.WORD], ["eur", Find.WORD], ["\(E\)", Find.REGEX]], name): points += 50
+    if _findWords([["japan", Find.WORD], ["jap", Find.WORD], ["\(J\)", Find.REGEX]], name):   points += 10; isJap=True
+    if _findWords([["usa", Find.WORD], ["\(U\)", Find.REGEX], ["australia", Find.WORD]], name): points += 30
+    if _findWords([["world", Find.WORD]], name): points += 20
+    if _findWords([["beta", Find.WORD]], name): points -=30
+    if _findWords([["\(proto.*?\)", Find.REGEX]], name): points -= 50
+    if _findWords([["spain", Find.WORD]], name): points += 100
+    if _findWords([["\[!\]", Find.REGEX]], name): points += 40
+    if _findWords([["\[b\d{0,2}\]", Find.REGEX]], name): points -= 40
+    if _findWords([["\[h\d{0,2}\]", Find.REGEX]], name): points -= 100
+    if _findWords([["\(UE\)", Find.REGEX], ["\(EU\)", Find.REGEX]], name): points+=50
+    if _findWords([["\(UJ\)", Find.REGEX], ["(JU)", Find.REGEX]], name): points += 30
+    if _findWords([["\[a\d{0,2}(?![-\w])\]", Find.REGEX]], name): points-=10
+    if _findWords([["\[o\d{0,2}(?![-\w])\]", Find.REGEX]], name): points -= 10 #Overdump
 
-    if findWords([["\[t\-eng.*?\]", Find.REGEX]], name):
+    if _findWords([["\[t\-eng.*?\]", Find.REGEX]], name):
         points += 50
-    elif findWords([["\[t\-spa.*?\]", Find.REGEX]], name): points += 60
-    elif findWords([["\[t\-.*?\]", Find.REGEX]], name):
+    elif _findWords([["\[t\-spa.*?\]", Find.REGEX]], name): points += 60
+    elif _findWords([["\[t\-.*?\]", Find.REGEX]], name):
         if isJap:
             points += 40
         else:
             points -=40
 
-    if findWords([["partial", Find.REGEX]], name):
+    if _findWords([["partial", Find.REGEX]], name):
         points-=10
 
-    if findWords([["\[t\d{0,2}(?![-\w])\]", Find.REGEX]], name): points -= 20 #No trainers please
+    if _findWords([["\[t\d{0,2}(?![-\w])\]", Find.REGEX]], name): points -= 20 #No trainers please
 
     return points
 
-def calculateSizeOfFilesInBytes(gameFiles:List[GameFile]):
+def _calculateSizeOfFilesInBytes(gameFiles:List[GameFile]):
     totalSize = 0
     for gameFile in gameFiles:
         totalSize += os.path.getsize(gameFile.absPath)
 
     return totalSize
 
-def convertBytesToMBStr(sizeInBytes:int) -> str:
+def _convertBytesToMBStr(sizeInBytes:int) -> str:
     size = str (round ((sizeInBytes/(1024*1024)),1)) + ' MB'
     return size
 
-def printReport(gneralReport:bool, clonesReport:bool, totalFileCount:int, totalFileSize:int, bestVersSize:int, gameGroups:[],bestVers:[]):
-    if gneralReport or clonesReport:
+def _dedupFolderPrintReport(generalReport:bool, clonesReport:bool, totalFileCount:int, totalFileSize:int, bestVersSize:int, gameGroups:[], bestVers:[]):
+    if generalReport or clonesReport:
         print("Report")
         print("=========================================================================")
 
-        if gneralReport:
+        if generalReport:
             print("General Data...")
             print("===============")
 
-            print("# Originals ->  File Count: %s Total Size: %s" % (totalFileCount, convertBytesToMBStr(totalFileSize)))
-            print("# De duplicated -> File Count: %s TotalSize: %s" % (len(fileGroups), convertBytesToMBStr(bestVersSize)))
-            print("# Total games removed: %s Total space saved: %s" % (totalFileCount - len(fileGroups), convertBytesToMBStr(totalFileSize - bestVersSize)))
+            print("# Originals ->  File Count: %s Total Size: %s" % (totalFileCount, _convertBytesToMBStr(totalFileSize)))
+            print("# De duplicated -> File Count: %s TotalSize: %s" % (len(gameGroups), _convertBytesToMBStr(bestVersSize)))
+            print("# Total games removed: %s Total space saved: %s" % (totalFileCount - len(gameGroups), _convertBytesToMBStr(totalFileSize - bestVersSize)))
 
         if clonesReport:
             print("Info: Writing clones report...")
@@ -181,14 +188,51 @@ def printReport(gneralReport:bool, clonesReport:bool, totalFileCount:int, totalF
                             print("## [ ] %s" % game.absPath)
                     print("##################################################################################")
 
+def _dedupGameListPrintReport(generalReport:bool, clonesReport:bool,gamesTotalCount:int, gamesTotalSize:int, gameGroups:[], bestVers:[], nonBestVersGames:[]):
+    if generalReport or clonesReport:
+        nonBestVersGamesSize = _calculateSizeOfFilesInBytes(nonBestVersGames)
+        nonBestVersGamesCount = len(nonBestVersGames)
+
+        print("Report")
+        print("=========================================================================")
+
+        if generalReport:
+            print("General Data...")
+            print("===============")
+
+            print("# Originals ->  File Count: %s Total Size: %s" % (gamesTotalCount, _convertBytesToMBStr(gamesTotalSize)))
+            print("# Removed %s games and you saved %s" % (nonBestVersGamesCount, _convertBytesToMBStr(nonBestVersGamesSize)))
+
+        if clonesReport:
+            print("Info: Writing clones report...")
+            print("==============================")
+
+            for i, bestVer in enumerate(bestVers):
+                group = gameGroups[i]
+                if len(group.games) == 1:
+                    print("## Game with no clones: %s" % group.games[0].absPath)
+                else:
+                    print("##################################################################################")
+                    for game in group.games:
+                        if game == bestVer:
+                            print("## [\u2713] %s" % game.absPath)
+                        else:
+                            print("## [X] %s" % game.absPath)
+                    print("##################################################################################")
+
 def _calculateDuplicates(games:List[GameFile]) -> List[GameGroup]:
     print("Calculating duplicates...")
+    clonedGameList = [];
+    clonedGameList.extend(games)
+    games=clonedGameList
+
+    gameGroups = []
 
     while len(games) > 0:
         if len(games) == 1:
             group = GameGroup()
             group.addGame(games[0])
-            fileGroups.append(group)
+            gameGroups.append(group)
             break;
 
         file = games[0]
@@ -201,15 +245,41 @@ def _calculateDuplicates(games:List[GameFile]) -> List[GameGroup]:
             if Comparers.exactCompare(fileName, choice):
                 group.addGame(games[index])
 
-        fileGroups.append(group)
+        gameGroups.append(group)
 
         for game in group.games:
             games.remove(game)
 
-    return fileGroups
+    return gameGroups
+
+def _copyFiles(outputFolder:str, games:[], exportRemoveCharCount:int):
+    _copyAndMoveFiles(outputFolder,games,exportRemoveCharCount,True)
+
+def _moveFiles(outputFolder:str, games:[], exportRemoveCharCount:int):
+    _copyAndMoveFiles(outputFolder,games,exportRemoveCharCount,False)
+
+def _copyAndMoveFiles(outputFolder:str, games:[], exportRemoveCharCount:int, copyAction:bool=True):
+        copy: bool = True
+        if not os.path.exists(outputFolder):
+            os.makedirs(outputFolder)
+        else:
+            filesInFolderCount = len(fnmatch.filter(os.listdir(outputFolder), '*'))
+            if filesInFolderCount > 0:
+                print("ERROR: The output folder is not empty. Please delete it or its contains to export games")
+                copy = False
+
+        if copy:
+            for i, game in enumerate(games):
+                exportingName = game.baseName[exportRemoveCharCount:]
+                if copyAction:
+                    print("Copying %s to %s (%s/%s)" % (game.absPath, outputFolder, i+1, len(games)))
+                    shutil.copy(game.absPath, os.path.join(outputFolder, exportingName))
+                else:
+                    print("Moving %s to %s (%s/%s)" % (game.absPath, outputFolder, i+1, len(games)))
+                    shutil.move(game.absPath, os.path.join(outputFolder, exportingName))
 
 def dedupFolder(args):
-    searchPath = args.game_folder
+    searchPath = args.rom_folder
     outputPath = args.output_folder
 
     extensionList = args.extensions.split(",")
@@ -217,6 +287,7 @@ def dedupFolder(args):
     compareRemoveCharCount = args.compare_remove
     exportRemoveCharCount = args.export_remove
 
+    gameGroups=[]
     games: List[GameFile] = []
 
     for ext in extensionList:
@@ -224,9 +295,9 @@ def dedupFolder(args):
 
         for file in filesGenerator:
             absPath: str = str(file)
-            games.append(GameFile(absPath, getNameToCompare(os.path.basename(absPath), compareRemoveCharCount)))
+            games.append(GameFile(absPath, _getNameToCompare(os.path.basename(absPath), compareRemoveCharCount)))
 
-    totalFileSize = calculateSizeOfFilesInBytes(games)
+    totalFileSize = _calculateSizeOfFilesInBytes(games)
 
     totalFileCount: int = len(games)
 
@@ -239,53 +310,8 @@ def dedupFolder(args):
         print("Deleting output folder %s" % outputPath)
         shutil.rmtree(outputPath)
 
-
-
-    print("Found %s duplicates..." % (totalFileCount - len(fileGroups)))
-
-    bestVers: [] = []
-
-    print("Calculating best versions...")
-    for group in fileGroups:
-        bestVer = group.getBestVersion()
-        bestVers.append(bestVer)
-
-    bestVersSize: int = calculateSizeOfFilesInBytes(bestVers)
-
-    if args.output_folder:
-        print("Exporting files to %s" % args.output_folder)
-        copy: bool = True
-        if not os.path.exists(args.output_folder):
-            os.makedirs(args.output_folder)
-        else:
-            filesInFolderCount = len(fnmatch.filter(os.listdir(args.output_folder), '*'))
-            if filesInFolderCount > 0:
-                print("ERROR: The output folder is not empty. Please delete it or its contains to export games")
-                copy = False
-
-        if copy:
-            for i, bestVer in enumerate(bestVers):
-                print("Copying %s to %s (%s/%s)" % (bestVer.absPath, outputPath, i, len(bestVers)))
-                exportingName = bestVer.baseName[exportRemoveCharCount:]
-                shutil.copy(bestVer.absPath, os.path.join(outputPath, exportingName))
-
-    printReport(args.general_report, args.clones_report,totalFileCount, totalFileSize, bestVersSize,bestVers)
-
-    print("Done!")
-
-def dedupGameList(args):
-    tree = ET.parse('gamelist.xml')
-    root = tree.getroot()
-
-    elements = root.findall('./game')
-
-    games = []
-    for element in elements:
-        path = element.find("path").text
-        name = element.find("name").text
-        games.append(GameFile(path,name))
-
     gameGroups = _calculateDuplicates(games)
+    print("Found %s duplicates..." % (totalFileCount - len(gameGroups)))
 
     bestVers: [] = []
 
@@ -294,5 +320,72 @@ def dedupGameList(args):
         bestVer = group.getBestVersion()
         bestVers.append(bestVer)
 
+    bestVersSize: int = _calculateSizeOfFilesInBytes(bestVers)
 
-    printReport(True, True,  0, 0,0,gameGroups, bestVers)
+    if args.output_folder:
+        print("Exporting files to %s" % args.output_folder)
+        _copyFiles(args.output_folder, bestVers, exportRemoveCharCount)
+
+
+    _dedupFolderPrintReport(args.general_report, args.clones_report, totalFileCount, totalFileSize, bestVersSize, gameGroups, bestVers)
+
+    print("Done!")
+
+def dedupGameList(args):
+    gameList:str=args.game_list
+    romFolder:str=args.rom_folder
+    outputFolder:str=args.output_folder
+    reportGeneral:bool=args.general_report
+    clonesResport:bool=args.clones_report
+    deleteOutputFolder:bool=args.delete_output_folder
+
+    if not os.path.exists(gameList):
+        print("Error: Game list file does not exist at %s" % gameList)
+        exit(1)
+
+    tree = ET.parse(gameList)
+    root = tree.getroot()
+
+    elements = root.findall('./game')
+
+    games = []
+    for element in elements:
+        path = element.find("path").text
+        name = element.find("name").text
+        path = os.path.join(romFolder,os.path.basename(path))
+        games.append(GameFile(path,name))
+
+
+    totalFileCount: int = len(games)
+
+    if totalFileCount == 0:
+        print("Could not find any rom to process in %s." % romFolder)
+        print("Done.")
+        exit(0)
+
+    if deleteOutputFolder and os.path.exists(outputFolder):
+        print("Deleting output folder %s" % outputFolder)
+        shutil.rmtree(outputFolder)
+
+    gameGroups = _calculateDuplicates(games)
+
+    bestVers: [] = []
+    gamesToRemove: [] = []
+    print("Calculating best versions...")
+    for group in gameGroups:
+        bestVer = group.getBestVersion()
+        bestVers.append(bestVer)
+
+    for group in gameGroups:
+        nonBestVersionGames = group.getNonBestVersionGames()
+        gamesToRemove.extend(nonBestVersionGames)
+
+    gamesTotalSize = _calculateSizeOfFilesInBytes(games)
+    gamesTotalCount = len(games)
+
+    _dedupGameListPrintReport(reportGeneral, clonesResport,gamesTotalCount, gamesTotalSize, gameGroups, bestVers, gamesToRemove)
+
+    if outputFolder:
+        _moveFiles(outputFolder, gamesToRemove, 0)
+
+    print("Done!")
--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import pathlib
 
 import deduper
 
-'''
+
 def dedupFolder(args):
-    dedupFolder(args)
+    deduper.dedupFolder(args)
 
 def dedupGameList(args):
-    dedupGameList(args)
+    deduper.dedupGameList(args)
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
 parserA = subparsers.add_parser('dedup_folder',help='Given a folder with roms we dedup it and send the output roms to a folder')
 parserA.set_defaults(func=dedupFolder)
-parserA.add_argument('--game-folder', type=pathlib.Path, required=True, help='Game folder')
-parserA.add_argument('--output-folder', type=pathlib.Path, required=False, help='Good games output folder. If it is not provided no clean romset export witll be done')
+parserA.add_argument('--rom-folder', type=pathlib.Path, required=True, help='Folder with roms')
+parserA.add_argument('--output-folder', type=pathlib.Path, required=False, help='Good games output folder. The copying of good roms will be performed if this param is given')
 parserA.add_argument('--general-report', required=False, action='store_true', help='Print general report')
 parserA.add_argument('--clones-report', required=False, action='store_true', help='Print clones report')
 parserA.add_argument('--report-to-file', required=False, action='store_true', help='Write report to file too')
@@ -25,16 +25,15 @@ parserA.add_argument('--compare-remove', required=False, type=int, default=0, he
 parserA.add_argument('--export-remove', required=False, type=int,default=0, help="How many characters should I remove at the beginning to export games")
 parserA.add_argument('--delete-output-folder', required=False, action='store_true',default=0, help="Delete output folder before exporting games")
 
-
 parserB = subparsers.add_parser('dedup_gamelist',help='Given a gamelist in Emulation station format we dedup the system moving duplicates to an output folder')
-parserB.add_argument('--output-folder', type=pathlib.Path, required=False, help='Good games output folder. If it is not provided no clean romset export witll be done')
+parserB.set_defaults(func=dedupGameList)
+parserB.add_argument('--game-list', type=pathlib.Path, required=True, help='Path to gamelist')
+parserB.add_argument('--rom-folder', type=pathlib.Path, required=True, help='Path to rom folder related to the gamelist')
+parserB.add_argument('--output-folder', type=pathlib.Path, required=False, help='Removed roms folder. The movement of roms will be performed if this param is given')
 parserB.add_argument('--general-report', required=False, action='store_true', help='Print general report')
 parserB.add_argument('--clones-report', required=False, action='store_true', help='Print clones report')
-parserB.add_argument('--delete-output-folder', required=False, action='store_true',default=0, help="Delete output folder before exporting games")
+parserB.add_argument('--delete-output-folder', required=False, action='store_true',default=0, help="Delete output folder before outputing duplicated roms")
 
 args = parser.parse_args()
 if hasattr(args, 'func'):
     args.func(args)
-
-'''
-deduper.dedupGameList([])
